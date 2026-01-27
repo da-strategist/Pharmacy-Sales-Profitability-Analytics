35012845d5b77a2e0f25f28c5b72e2ae1b5668fc
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2026-01-27 13:56:04 +0200

    promo performance int model

--- models/example/intermidiate/int_promo_perf_fact.sql
+++ models/example/intermidiate/int_promo_perf_fact.sql
@@ -1,11 +1,12 @@
+{% set promoflag = ['Yes', 'No'] %}
 
-WITH promo_etx as (
+WITH promo_ext as (
 
         SELECT 
             product_id,
             monthkey,
             units_sold,
-            total_revenue,
+            total_rev as total_revenue,
             total_cost,
             promoflag
         FROM {{ref('int_product_perf_fact')}}
@@ -15,5 +16,11 @@ WITH promo_etx as (
         SELECT 
             product_id,
             monthkey,
-            
-)
+            SUM(case when promoflag = 'Yes' THEN units_sold end) as promo_units_sold,
+            SUM(case when promoflag = 'No' THEN units_sold end) as non_promo_units_sold,
+            SUM(case when promoflag = 'Yes' THEN total_revenue end) as promo_revenue,
+            SUM(case when promoflag = 'No' THEN total_revenue end) as non_promo_revenue
+        FROM promo_ext
+        GROUP BY product_id, monthkey
+
+) SELECT * FROM promo_fact
