7fead69435fcd74734120061be88718e4073ce8c
Jubril Olalekan <jubril.d.olalekan@gmail.com>
2026-01-28 14:32:49 +0200

    promo update

--- models/example/mart/promo_performance_mart/m_promo_performance.sql
+++ models/example/mart/promo_performance_mart/m_promo_performance.sql
@@ -11,8 +11,8 @@ WITH promo_eext as (
             sum(case when promoflag = 'No' THEN units_sold END) as non_promo_units_sold,
             sum(case when promoflag = 'Yes' THEN total_revenue END) as promo_revenue,
             sum(case when promoflag = 'No' THEN total_revenue END) as non_promo_revenue,
-           -- sum(case when promoflag = 'Yes' THEN total_cost END) as promo_cost,
-            --sum(case when promoflag = 'No' THEN total_cost END) as non_promo_cost,
+            sum(case when promoflag = 'Yes' THEN total_cost END) as promo_cost,
+            sum(case when promoflag = 'No' THEN total_cost END) as non_promo_cost,
             sum(case when promoflag = 'Yes' THEN total_margin END) as promo_margin,
             sum(case when promoflag = 'No' THEN total_margin END) as non_promo_margin
     FROM {{ref('int_promo_perf_fact')}}
